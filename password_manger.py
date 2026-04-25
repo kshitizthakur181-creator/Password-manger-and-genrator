9f858393b2367c2dfbf8e3350827db95a1fcc411
Kshitiz Thakur <kshitizthakur181@gmail.com>
2026-04-26 03:00:02 +0530

    words to be used

--- password_manger.py
+++ password_manger.py
@@ -3,8 +3,11 @@ import string
 
 
 #About Password
-
-
+lowercase = string.ascii_lowercase 
+uppercase = string.ascii_uppercase
+digits = string.digits
+punctuations = string.punctuation
+character_used = None
 def genrate_password():
     pass
 
@@ -20,8 +23,10 @@ def save_password():
 
 # Main function 
 def main():
-    pass
-
-
+    length = int(input("Enter the Length for the password  : "))
+    type_used =input("Enter the words to be use [Lower case (low), Upper Case (up) , Digits (digits) , punctutaions (punc)] : ")
+    type_used = type_used.split()
+    print(type_used)
+    
 if __name__ == "__main__" :
     main()
