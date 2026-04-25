d697086a959f74fe1f06e9cc5441d9970e94cea2
Kshitiz Thakur <kshitizthakur181@gmail.com>
2026-04-26 03:24:13 +0530

    genrating password

--- password_manger.py
+++ password_manger.py
@@ -2,31 +2,51 @@ import random
 import string
 
 
-#About Password
-lowercase = string.ascii_lowercase 
+# About Password
+lowercase = string.ascii_lowercase
 uppercase = string.ascii_uppercase
 digits = string.digits
 punctuations = string.punctuation
-character_used = None
-def genrate_password():
-    pass
+
+
+def genrate_password(character_used, length):
+    password = "".join(random.choices(character_used, k=length))
+    print(password)
 
 
 def view_password():
     pass
 
-def save_password():
+
+def save_password(password):
     pass
 
 
-
-
-# Main function 
+# Main function
 def main():
-    length = int(input("Enter the Length for the password  : "))
-    type_used =input("Enter the words to be use [Lower case (low), Upper Case (up) , Digits (digits) , punctutaions (punc)] : ")
+
+    length = int(input("Enter the Length for the password (8 - 16) : "))
+    type_used = input(
+        "Enter the words to be use [Lower case (low), Upper Case (up) , Digits (digits) , punctutaions (punc)] : "
+    )
     type_used = type_used.split()
-    print(type_used)
     
-if __name__ == "__main__" :
+    character_used = ""
+    
+    for typies in type_used:
+        if typies == "low":
+            character_used += lowercase
+        elif typies == "up":
+            character_used += uppercase
+        elif typies == "digits":
+            character_used += digits
+        elif typies == "punc":
+            character_used += punctuations
+        else:
+            print(" ---Invalid word--- ")
+            break
+    genrate_password(character_used, length)
+
+
+if __name__ == "__main__":
     main()
