8a9af6b34d4243327f3ca04103abea2cc399c6c9
Kshitiz Thakur <kshitizthakur181@gmail.com>
2026-04-26 01:09:36 +0530

    Main format

---
+++ password_manger.py
@@ -0,0 +1,27 @@
+import random
+import string
+
+
+#About Password
+
+
+def genrate_password():
+    pass
+
+
+def view_password():
+    pass
+
+def save_password():
+    pass
+
+
+
+
+# Main function 
+def main():
+    pass
+
+
+if __name__ == "__main__" :
+    main()
